8152dd7ba35cbb46f2d9211e9c3e12d4152022f2
Adolfo Lopez <alopezcia@gmail.com>
2024-04-18 18:42:56 +0200

    Insert into SQLite insertdb.js

---
+++ helppers/insertdb.js
@@ -0,0 +1,26 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const insertdb = (solicitud) =>{
+    const dbName = `./dbCert/inscripciones.db3`;
+    const db = new sqlite3.Database(dbName);
+    const { parroquia, nivel, apellidos, nombre, bautizo, fechaNacimiento, colegio, curso, profesor, 
+            apellidosPadre, nombrePadre, telefonoPadre, dniPadre, apellidosMadre, nombreMadre, telefonoMadre, dniMadre, 
+            direccion, codpost, email, alergias, comentarios, imagenDigital, imagenImpresa, comunicaciones, datosSalud, uuid } = solicitud;
+            
+    const qry = `INSERT INTO inscripciones(uuid, parroquia, nivel, apellidosn, nombren, bautizo, nacimiento, 
+                    colegio, curso, profesor, apellidosp, nombrep, telefonop, dnip, 
+                    apellidosm, nombrem, telefonom, dnim, direccion, codpost, email, 
+                    alergias, comentarios, protdatos1, protdatos2, protdatos3, protdatos4 )       
+                    VALUES('${uuid}','${parroquia}','${nivel}', '${apellidos}', '${nombre}', '${bautizo}', '${fechaNacimiento}', '${colegio}',
+                    '${curso}', '${profesor}', '${apellidosPadre}', '${nombrePadre}', '${telefonoPadre}', '${dniPadre}', 
+                    '${apellidosMadre}', '${nombreMadre}', '${telefonoMadre}', '${dniMadre}', 
+                    '${direccion}', '${codpost}', '${email}', '${alergias}', '${comentarios}', 
+                    '${imagenDigital}', '${imagenImpresa}', '${comunicaciones}', '${datosSalud}')`;
+    
+    db.run(qry);
+    db.close();
+}
+
+module.exports = {
+    insertdb
+}
