5f564031d0cc25ba303062295714a57d8804b598
Adolfo Lopez <alopezcia@gmail.com>
2024-04-22 19:59:33 +0200

    cert.js: Se eliminan insertar en SQLite certificados de usuarios

--- cert.js
+++ cert.js
@@ -4,15 +4,8 @@ const helmet  = require('helmet');
 var compression = require('compression');
 const cors = require('cors');
 require('dotenv').config();
-const sqlite3 = require('sqlite3').verbose();
 const fs = require('fs');
-const admzip = require('adm-zip');
 const qr = require("qrcode");
-
-const fecha = new Date().toJSON().slice(0,10).replaceAll('-', '').replaceAll('/','');
-const dbName = `./dbCert/${fecha}_cert.db3`;
-const allDbZip = './dbCert/allDbCertZip.zip';
-
 
 const httpsServerOptions = {
     key:  fs.readFileSync(process.env.KEY_PATH),
@@ -20,39 +13,6 @@ const httpsServerOptions = {
     requestCert: true,
     rejectUnauthorized: true
 };
-
-// Borrar el zip de todas las db para volver a crearlo
-if( fs.existsSync(allDbZip) ){ 
-    try{
-        fs.unlinkSync(allDbZip);
-    }catch( err ){
-        console.log(err);
-    }
-}
-// Crear el zip de todas las db3 
-try {
-    const zip = new admzip();
-    zip.addLocalFolder('./dbCert');
-    zip.writeZip( allDbZip )
-    console.log(`Creado fichero comprimido ${allDbZip}`);    
-} catch( err ){
-    console.log(err);
-}
-
-if( !fs.existsSync(dbName)){
-    fs.open( dbName, 'w', (err, file ) =>{
-        if (err) throw err;
-        console.log(`File ${dbName} is opened in write mode.`);
-        const db = new sqlite3.Database(dbName);
-        const createTable = 
-            'CREATE TABLE IF NOT EXISTS certs('+
-                'uuid varchar(36) PRIMARY KEY, '+
-                'cert text NOT NULL, '+
-                'registro DATETIME DEFAULT CURRENT_TIMESTAMP)';
-        db.run(createTable);
-        db.close();
-    });
-}
 
 const app = express();
 
@@ -103,37 +63,12 @@ app.get('/api/get-QRCert', async (req, res) => {
     const {subject, issuer, valid_to, serialNumber } = cert;
     const toQR = { subject, issuer, valid_to, serialNumber };
     const b64  = cert.raw.toString('base64');
-    const db = new sqlite3.Database(dbName);
-    const qry = `INSERT INTO certs(uuid, cert) VALUES('${uuid}','${b64}')`;
-    db.run(qry);
-    db.close();
     qr.toDataURL(JSON.stringify(toQR), (err, src) => {
         if (err) res.send("Error occured");
       
         // Let us return the QR code image as our response and set it to be the source used in the webpage
         res.send( src );
     });
-});
-
-
-app.get('/api/downloadDb', (req, res) => { 
-    console.log(`Solicitada descarga del fichero ${dbName}`);
-    if( fs.existsSync(dbName)){
-        res.download(dbName, (err)=>{
-            res.status(404).send(err);
-        });
-    } else 
-        res.status(404).send(`Error 404 - ${dbName} no encontrado`);
-});
-
-app.get('/api/downloadAll', (req, res) => { 
-    console.log(`Solicitada descarga del fichero ${allDbZip}`);
-    if( fs.existsSync(allDbZip)){
-        res.download(allDbZip, (err)=>{
-            res.status(404).send(err);
-        });
-    } else 
-        res.status(404).send(`Error 404 - ${allDbZip} no encontrado`);
 });
 
 
